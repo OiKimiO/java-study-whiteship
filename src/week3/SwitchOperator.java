b823316915069c96436985c29d79c1b55e1aafaf
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2023-07-11 18:12:09 +0900

    Add #15 - 자바 12 이전 Switch break를 쉽게 놓칠 수도 있다.
    - 빨간색인데 break를 안걸어 파란색의 값이 들어갑니다.

--- src/week3/SwitchOperator.java
+++ src/week3/SwitchOperator.java
@@ -7,6 +7,7 @@ public class SwitchOperator {
 
     private static void java12이전_Switch() {
         System.out.println(코드가길다());
+        System.out.println(break를_놓친다());
     }
 
     private static int 코드가길다() {
@@ -14,6 +15,21 @@ public class SwitchOperator {
         int temp = 0;
 
         switch(요청값){
+            case "파란색":
+                temp = 1;
+                break;
+        }
+
+        return temp;
+    }
+
+    private static int break를_놓친다() {
+        String 요청값 = "파란색";
+        int temp = 0;
+
+        switch(요청값){
+            case "빨간색":
+                temp = 2;
             case "파란색":
                 temp = 1;
                 break;
