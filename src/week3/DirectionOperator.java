90158c4ac043333715431f4bd6f7feb8af751935
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2023-07-11 11:27:38 +0900

    Add #9 - 람다표현식_중괄호없음

--- src/week3/DirectionOperator.java
+++ src/week3/DirectionOperator.java
@@ -1,6 +1,7 @@
 package week3;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
 
 public class DirectionOperator {
@@ -12,6 +13,8 @@ public class DirectionOperator {
         익명클래스();
         람다표현식();
         람다표현식_매개변수하나_테스트();
+        람다표현식_중괄호_없음_테스트();
+
     }
 
     private static Object 익명클래스() {
@@ -51,6 +54,21 @@ public class DirectionOperator {
         return s ->  System.out.println("Hello, World! " + s);
     }
 
-    
+    private static void 람다표현식_중괄호_없음_테스트() {
+        Consumer<String> 일반_consumer = 람다표현식_몸체중괄호();
+        일반_consumer.accept("은호");
 
+        Consumer<String> 람다_consumer = 람다표현식_몸체중괄호_없음();
+        람다_consumer.accept("은호");
+    }
+
+    private static Consumer<String> 람다표현식_몸체중괄호() {
+        return s -> {
+            System.out.println("Hello, World! " + s);
+        };
+    }
+
+    private static Consumer<String> 람다표현식_몸체중괄호_없음() {
+        return s -> System.out.println("Hello, World! " + s);
+    }
 }
