1c0e0ed57225739b1efaf7fbaa927c79d8961e08
OiKimiO <53805469+OiKimiO@users.noreply.github.com>
2023-07-11 11:08:21 +0900

    Add #8 - 람다표현식_매개변수하나
    - 매개변수가 하나인 함수형 인터페이스인 Consumer를 사용했습니다.
    - 일반 표현식과 람다 표현식을 구분했습니다.
    - 각 표현식을 테스트하였습니다.

--- src/week3/DirectionOperator.java
+++ src/week3/DirectionOperator.java
@@ -1,5 +1,6 @@
 package week3;
 
+import java.util.function.Consumer;
 import java.util.function.IntBinaryOperator;
 
 public class DirectionOperator {
@@ -10,6 +11,7 @@ public class DirectionOperator {
     private static void 람다() {
         익명클래스();
         람다표현식();
+        람다표현식_매개변수하나_테스트();
     }
 
     private static Object 익명클래스() {
@@ -21,12 +23,34 @@ public class DirectionOperator {
     }
 
     private static IntBinaryOperator 람다표현식() {
-        return (int x,int y) -> x < y ? x : y;
+        return (int x, int y) -> x < y ? x : y;
     }
 
     private static IntBinaryOperator 람다표현식_타입추론가능() {
         return (x, y) -> x < y ? x : y;
     }
 
+    private static void 람다표현식_매개변수하나_테스트() {
+        Consumer<String> 일반_consumer = 일반표현식_매개변수하나();
+        일반_consumer.accept("은호");
+
+        Consumer<String> 람다_consumer = 람다표현식_매개변수하나();
+        람다_consumer.accept("은호");
+    }
+
+    private static Consumer<String> 일반표현식_매개변수하나(){
+        return new Consumer<String>(){
+            @Override
+            public void accept(String s) {
+                System.out.println("Hello, World! " + s);
+            }
+        };
+    }
+
+    private static Consumer<String> 람다표현식_매개변수하나() {
+        return s ->  System.out.println("Hello, World! " + s);
+    }
+
+    
 
 }
