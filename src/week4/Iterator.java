6664ca0f3b3f73924f1e046b53856697456903c7
OiKimiO <etkim02@naver.com>
2023-07-12 22:30:24 +0900

    Add #23 - for문 메서드를 작성

--- src/week4/Iterator.java
+++ src/week4/Iterator.java
@@ -5,7 +5,8 @@ import java.util.Stack;
 public class Iterator {
     public static void main(String[] args) {
         // while문();
-        doWhile문();
+        // doWhile문();
+        for문();
     }
 
     private static void while문() {
@@ -29,5 +30,12 @@ public class Iterator {
             System.out.println(stk.peek());
             stk.pop();
         }while (!stk.isEmpty());
+    }
+
+    private static void for문() {
+        int[] arr = new int[]{1,2};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
