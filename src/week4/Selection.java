7b9901a8fd1f6fb994d7a955a5200df6cf6f79e5
OiKimiO <etkim02@naver.com>
2023-07-12 22:18:06 +0900

    Add #18 - if문 메서드 작성

---
+++ src/week4/Selection.java
@@ -0,0 +1,14 @@
+package week4;
+
+public class Selection {
+    public static void main(String[] args) {
+        if문(true);
+    }
+
+    private static void if문(boolean 내일은_시험일) {
+        if(내일은_시험일){
+            System.out.println("집에 가서 공부해야죠!");
+        }
+    }
+
+}
