75ac49d9d31b969999ed27c953731059204a1ee5
OiKimiO <etkim02@naver.com>
2023-07-12 22:19:45 +0900

    Add #19 - ifElse문 메서드 작성

--- src/week4/Selection.java
+++ src/week4/Selection.java
@@ -2,7 +2,16 @@ package week4;
 
 public class Selection {
     public static void main(String[] args) {
-        if문(true);
+        // if문(true);
+        ifElse문(false);
+    }
+
+    private static void ifElse문(boolean 내일은_시험일) {
+        if(내일은_시험일){
+            System.out.println("집에 가서 공부해야죠!");
+        }else{
+            System.out.println("친구들이랑 놀 수도 있죠!");
+        }
     }
 
     private static void if문(boolean 내일은_시험일) {
