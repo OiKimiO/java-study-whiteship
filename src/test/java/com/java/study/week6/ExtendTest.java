eccd1325915ba337f978f926209b8a8a689c64eb
OiKimiO <etkim02@naver.com>
2023-08-15 16:56:27 +0900

    Add #40 - 상속을 테스트하라
    - 상속 중super를 사용해 보았습니다.

---
+++ src/test/java/com/java/study/week6/ExtendTest.java
@@ -0,0 +1,12 @@
+package com.java.study.week6;
+
+import org.junit.jupiter.api.Test;
+
+public class ExtendTest {
+
+    @Test
+    void super테스트(){
+        Child child = new Child();
+        child.childMethod();
+    }
+}
