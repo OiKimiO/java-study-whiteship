6eae880f9bbaadea5b584b68b7c8407f22f94421
OiKimiO <etkim02@naver.com>
2023-08-15 17:07:04 +0900

    Add #43 - 더블 디스패치를 테스트하라

--- src/test/java/com/java/study/week6/ExtendTest.java
+++ src/test/java/com/java/study/week6/ExtendTest.java
@@ -2,6 +2,9 @@ package com.java.study.week6;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ExtendTest {
 
     @Test
@@ -20,6 +23,14 @@ public class ExtendTest {
     void 오버라이딩_테스트2(){
         ParnetA a = new ParnetA(new ChildB());
         a.print(); // 실행 시점엔 ChildB를 넣어 B에서 재정의한 데이터를 출력
+    }
+
+    @Test
+    void 더블디스패치테스트(){
+        List<Post> posts = Arrays.asList(new Text(), new Picture());
+        List<SNS> sns = Arrays.asList(new Facebook(), new Twitter());
+
+        posts.forEach(p -> sns.forEach(s -> p.postOn(s)));
     }
 
 }
