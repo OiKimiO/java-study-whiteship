bb8dc27228638028dd6058fc519b22b25490df2c
OiKimiO <etkim02@naver.com>
2023-08-15 17:02:45 +0900

    Add #42 - 동적 메소드 디스패치를 테스트 하라

--- src/test/java/com/java/study/week6/ExtendTest.java
+++ src/test/java/com/java/study/week6/ExtendTest.java
@@ -15,4 +15,11 @@ public class ExtendTest {
         B b = new B();
         b.print(); // 재정의한 B값을 출력
     }
+
+    @Test
+    void 오버라이딩_테스트2(){
+        ParnetA a = new ParnetA(new ChildB());
+        a.print(); // 실행 시점엔 ChildB를 넣어 B에서 재정의한 데이터를 출력
+    }
+
 }
