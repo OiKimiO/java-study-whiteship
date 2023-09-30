75437acc73c8d9fdc512f38a413394b0ac748821
OiKimiO <etkim02@naver.com>
2023-09-30 13:36:56 +0900

    Modify #54 - Enum 예제7
    - Transport: 추상메서드를 통해 거리별 운임요금 계산하는 enum 클래스

--- src/main/java/com/java/study/week11/CompareData.java
+++ src/main/java/com/java/study/week11/CompareData.java
@@ -2,6 +2,6 @@ package com.java.study.week11;
 
 public class CompareData {
     public static void main(String[] args) {
-        System.out.println(Currency.DIME.value());
+        System.out.println(Transport.TAXI.fare(10));
     }
 }
--- src/main/java/com/java/study/week11/Transport.java
+++ src/main/java/com/java/study/week11/Transport.java
@@ -1,25 +1,31 @@
 package com.java.study.week11;
 
 public enum Transport {
-    BUS(1200), TAXI(3900), SUBWAY(1200);
-    private final int BASIC_FARE; // 기본요금
+    BUS(1200){
+        @Override
+        double fare(int distance){
+            return distance * BASIC_FARE * 1.5;
+        }
+    },
+
+    TAXI(3900){
+        @Override
+        double fare(int distance) {
+            return distance * BASIC_FARE * 2.0;
+        }
+    },
+
+    SUBWAY(1200){
+        @Override
+        double fare(int distance) {
+            return distance * BASIC_FARE * 0.5;
+        }
+    };
+    protected final int BASIC_FARE; // 기본요금, protected로 선언해야 상수에서 접근 가능
 
     Transport(int BASIC_FARE) {
         this.BASIC_FARE = BASIC_FARE;
     }
 
-    public double fare(){
-        switch (this){ // 운송 수단별로 다르게 책정되는 요금
-            case BUS -> {
-                return BASIC_FARE * 1.5;
-            }
-            case TAXI -> {
-                return BASIC_FARE * 2.0;
-            }
-            case SUBWAY -> {
-                return BASIC_FARE * 0.5;
-            }
-            default -> throw new IllegalArgumentException(); // 실행될 일 없느 코드이지만 없으면 컴파일 에러남
-        }
-    }
+    abstract double fare(int distance); // 거리에 따른 요금 계산
 }
